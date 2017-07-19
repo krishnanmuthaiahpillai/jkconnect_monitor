b90f13ba6aaf7d88fdfc074350b87f19ef3a7277
krishnan <krishnapillai87@gmail.com>
2017-07-19 12:51:28 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+setup(
+    name='jkconnect_monitor',
+    packages=['jkconnect_monitor'],
+    version='0.0.1',
+    description='A tshark wrapper to count the number of cellphones in the vicinity',
+    author='krishnan',
+    url='https://github.com/krishnanmuthaiahpillai/jkconnect_monitor',
+    author_email='krishnanmuthaiahpillai@gmail.com',
+    download_url='https://github.com/krishnanmuthaiahpillai/jkconnect_monitor/archive/v1.0.tar.gz',
+    keywords=['tshark', 'wifi', 'location'],
+    classifiers=[],
+    install_requires=[
+        "click",
+        "netifaces",
+        "pick",
+    ],
+    setup_requires=[],
+    tests_require=[],
+    entry_points={'console_scripts': [
+        'jkconnect_monitor = jkconnect_monitor.__main__:main',
+    ], },
+)
