0d82ea706f3713b1ba03b27a1323c1ccdd46fdf7
krishnan <krishnapillai87@gmail.com>
2017-07-26 17:07:11 +0530

    print added7

--- jkconnect_monitor/jk_db.py
+++ jkconnect_monitor/jk_db.py
@@ -1,10 +1,10 @@
 import json
-def parse_data(jsondata):
-    print data
+def parse_data(data_dump):
+    print data_dump
     #parsed_input = json.load(data)
     # print parsed_input
 
 if __name__ == '__main__':
     # test1.py executed as script
     # do something
-   parse_data(jsondata)
+   parse_data(data_dump)
