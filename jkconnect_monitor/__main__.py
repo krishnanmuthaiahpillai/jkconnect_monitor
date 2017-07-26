f639271be83aae45ff23341e4adacee41e649eb4
krishnan <krishnapillai87@gmail.com>
2017-07-26 12:56:25 +0530

    print added1

--- jkconnect_monitor/__main__.py
+++ jkconnect_monitor/__main__.py
@@ -261,6 +261,9 @@ def scan(adapter, scantime, verbose, number, nearby, jsonprint, out, allmacaddre
             data_dump = {'cellphones': cellphone_people, 'time': time.time()}
             print data_dump
             f.write(json.dumps(data_dump) + "\n")
+    else:
+            print 'NO OUT'
+        
         if verbose:
             print("Wrote %d records to %s" % (len(cellphone_people), out))
     os.remove('/tmp/tshark-temp')
