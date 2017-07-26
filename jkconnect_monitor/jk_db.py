8e748cee2705ee0e65c025971673937c7c69fe96
krishnan <krishnapillai87@gmail.com>
2017-07-26 18:04:58 +0530

    print added13

--- jkconnect_monitor/jk_db.py
+++ jkconnect_monitor/jk_db.py
@@ -3,7 +3,16 @@ def parse_data(data_dump):
     #print data_dump
     parsed_input = json.loads(data_dump)
     cellphones = parsed_input['cellphones']
-    print cellphones
+    #print cellphones
+    for element in parsed_input['cellphones']:
+        company = element['company']
+        time = datetime.datetime.fromtimestamp(int(parsed_input['time'])).strftime('%Y-%m-%d %H:%M:%S')
+        rssi=element['rssi']
+        mac=element['mac']
+        print time
+        print company
+        print rssi
+        print mac
 
 
 
